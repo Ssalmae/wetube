df843a891069203f0709792d1976b593eb166730
Ssalmae <spvpt44@naver.com>
2022-05-25 17:27:13 +0900

    router

---
+++ src/controllers/videoControllers.js
@@ -0,0 +1,12 @@
+export const trending = (req, res) => res.send("Home Page Videos");
+export const see = (req, res) => {
+  return res.send(`Watch Videos #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  res.send("Edit Videos");
+};
+export const search = (req, res) => res.send("Search Videos");
+export const upload = (req, res) => res.send("Upload Videos");
+export const deleteVideo = (req, res) => {
+  res.send("Delete Videos");
+};

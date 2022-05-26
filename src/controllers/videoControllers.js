89526cfbb1eee4a55697ccf640fa06f7d5335f5f
Ssalmae <spvpt44@naver.com>
2022-05-26 19:26:27 +0900

    template

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,9 +1,37 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "tower",
+      rating: 5,
+      comment: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "eve",
+      rating: 5,
+      comment: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "isaac",
+      rating: 5,
+      comment: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => {
-  return res.send(`Watch Videos #${req.params.id}`);
+  return res.render("watch");
 };
 export const edit = (req, res) => {
-  res.send("Edit Videos");
+  return res.render("edit");
 };
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Videos");
